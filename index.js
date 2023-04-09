7936d9f45150258b2a7564bee33fcc4f041a3aef
Burcu <burju7@yahoo.com>
2023-04-09 17:02:40 +0300

    assingment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// // Write your code here
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+
+let num3 = 12;
+let num4 = 8;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 20, 14, 15, 4);
